ec68d26745ad2beebd7eee5087c38f616f51dc55
Melperez19 <melperez19@gmailcom>
2018-12-29 21:24:23 -0500

    fully functioning quiz

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,201 +1,201 @@
-// ----------------------------------------------- Main Process of the quiz ---------------------------------------------------------//
-$(document).ready(function () {
-    
-    $("#start-button").on("click", function () {
-        console.log("start")
-        $("#start-button").remove();
-        timeRemaining();
-        loadQuestion(currentQuestion);
-    })
-    function loadQuestion(questionIndex) {
-        var q = questions[questionIndex];
-        questionEl.textContent = (questionIndex + 1) + ' ' + q.question;
-        opt1.textContent = q.option1;
-        opt2.textContent = q.option2;
-        opt3.textContent = q.option3;
-        opt4.textContent = q.option4;
-    };
-
-    $(".next-btn").on("click", function () {
-        console.log("next")
-        timeRemaining();
-        loadNextQuestion();
-        
-    })
-    
-
-    //Create the time remaining countdown
-    function timeRemaining() {
-        clearInterval(intervalId)
-        intervalId = setInterval(thirtySeconds, 1000);
-        
-    }
-
-
-    // End quiz if counter gets to 0 and display results
-    function thirtySeconds() {
-        counter--;
-        $("#timer").html(counter);
-        if (counter === 0) {
-            stop();
-            displayResults()
-        }
-
-    }
-
-    function stop() {
-        clearInterval(intervalId);
-    }
-
-    function displayResults() {
-        $(".results").append("<h3> You got " + correctAnswers + " correct </h3>");
-        $(".results").append("<h3> You got " + incorrectAnswers + " wrong </h3>");
-        console.log("displayResults")
-    }
-
-    function loadNextQuestion() {
-        var selectedOption = document.querySelector('input[type=radio]:checked');
-        if (!selectedOption) {
-            alert('Please select your answer');
-            return;
-        }
-        var answer = selectedOption.value;
-        if (questions[currentQuestion].answer == answer) {
-            correctAnswers++;
-        }
-        selectedOption.checked = false;
-        currentQuestion++;
-        if (currentQuestion == totalQuestions - 1) {
-            nextButton.textContent = 'Finish';
-        }
-        if (currentQuestion == totalQuestions) {
-            grid.style.display = 'none';
-            result.textContent = "<h3> You got " + correctAnswers + " correct </h3>";
-            result.textContent = "<h3> You got " + incorrectAnswers + " incorrect </h3>";
-            return;
-        }
-
-    }
-
-
-
-
-
-
-
-    // ------------------------------------  Q&A Array  -------------------------------------//
-    var questions = [
-
-        {
-            "question": "When was Walt Disney born?",
-            "option1": "December 5, 1901",
-            "option2": "March 30, 1900",
-            "option3": "July 17 1899",
-            "option4": "February 5, 1894",
-            "answer": "December 5, 1901"
-        },
-        {
-
-            "question": "Walt's character, Oswald, was a:",
-            "option1": "Cat",
-            "option2": "Rabbit",
-            "option3": "Frog",
-            "option4": "Dog",
-            "answer": "Rabbit"
-        },
-        {
-            "question": "Walt Disney's partner, Ub Iwerks, is known for creating which of these five characters?",
-            "option1": "Goofy",
-            "option2": "Dumbo",
-            "option3": "Pinocchio",
-            "option4": "Mickey",
-            "answer": "Mickey"
-        },
-        {
-            "question": "What was Walt Disney's first full length film?",
-            "option1": "Dumbo",
-            "option2": "Pinocchio",
-            "option3": "Snow White",
-            "option4": "Cinderella",
-            "answer": "Snow White"
-
-        },
-        {
-            "question": "Which of the following are not a Walt Disney quote?",
-            "option1": "It's kind of fun to do the impossible.",
-            "option2": "To succeed, you must first realize the dream.",
-            "option3": "I believe in being an innovator.",
-            "option4": "If you can dream it, you can do it.",
-            "answer": "To succeed, you must first realize the dream."
-
-        },
-        {
-            "question": "When did Disneyland open?",
-            "option1": "August 1, 1961",
-            "option2": "May 30, 1959",
-            "option3": "July 17, 1955",
-            "option4": "June 14, 1949",
-            "answer": "July 17, 1955"
-        },
-        {
-            "question": "Which of these celebrities did not host at least part of the opening day Disneyland broadcast?",
-            "option1": "Art Linkletter",
-            "option2": "Ronald Reagan",
-            "option3": "Kirk Douglas",
-            "option4": "Irene Dunne",
-            "answer": "Kirk Douglas"
-
-        },
-        {
-            "question": "Which was not one of the opening day rides at Disneyland?",
-            "option1": "Autopia",
-            "option2": "Mad Tea Party",
-            "option3": "Dumbo the Flying Elephant",
-            "option4": "The Jungle Cruise",
-            "answer": "Dumbo the Flying Elephant"
-
-        },
-        {
-            "question": "What terminal illness did Walt Disney suffer from",
-            "option1": "Lung Cancer",
-            "option2": "Heat Stroke",
-            "option3": "Pneumonia",
-            "option4": "Brain Cancer",
-            "answer": "Lung Cancer"
-
-        },
-        {
-            "question": "Last Question! You're a trooper! When did Walt Disney World open?",
-            "option1": "November 19, 1980",
-            "option2": "July 17, 1970",
-            "option3": "June 3, 1981",
-            "option4": "October 1, 1971",
-            "answer": "October 1, 1971"
-
-        },
-    ]
-
-    //All other variables to keep track of time, questions, and scores
-
-    var currentQuestion = 0;
-    var counter = 31;
-    var correctAnswers = 0;
-    var incorrectAnswers = 0;
-    var totalQuestions = questions.length;
-    var intervalId;
-    var selectedOption;
-    var grid = document.getElementById('grid');
-    var questionEl = document.getElementById('question');
-    var opt1 = document.getElementById('opt1');
-    var opt2 = document.getElementById('opt2');
-    var opt3 = document.getElementById('opt3');
-    var opt4 = document.getElementById('opt4');
-    var nextButton = document.getElementById('nextButton')
-    result = document.getElementById('results');
-    var correctSound = new Audio("assets/sounds/fairy.mp3");
-    var incorrectSound = new Audio("assets/sounds/womp-womp.mp3");
-
-
-
+//Game starts on a intro page. Clicking the startButton starts the quiz and resets the page
+$("#startButton").on("click", function () {
+  $("#startButton").remove();
+  $("#instructions").remove();
+  quiz.loadQuestion();
 })
+
+$(document).on("click", ".option-button", function (e) {
+  quiz.clicked(e);
+})
+
+$(document).on("click", "#reset", function () {
+  quiz.reset();
+})
+
+// --------------------------Question & Answers Array----------------------------------
+var questions = [
+  {
+    question: "When was Walt Disney born?",
+    options: ["A: December 5, 1901", "B: March 30, 1900", "C: July 17 1899", "D: February 5, 1894"],
+    answer: "A: December 5, 1901",
+    image: "assets/images/tinkerbell.gif"
+  },
+  {
+    question: "Walt's character, Oswald, was a:",
+    options: ["A: Cat", "B: Rabbit", "C: Frog", "D: Dog"],
+    answer: "B: Rabbit",
+    image:"assets/images/oswald.gif"
+  },
+  {
+    question: "Walt Disney's partner, Ub Iwerks, is known for creating which of these five characters?",
+    options: ["A: Goofy", "B: Dumbo", "C: Pinocchio", "D: Mickey"],
+    answer: "D: Mickey",
+    image:"assets/images/mickey.gif"
+  },
+  {
+    question: "What was Walt Disney's first full length film?",
+    options: ["A: Dumbo", "B: Pinocchio", "C: Snow White", "D: Cinderella"],
+    answer: "C: Snow White",
+    image:"assets/images/snow_white.gif"
+  },
+  {
+    question: "Which of the following are not a Walt Disney quote?",
+    options: ["A: It's kind of fun to do the impossible.",
+      "B: To succeed, you must first realize the dream.",
+      "C: I believe in being an innovator.",
+      "D: If you can dream it, you can do it."],
+    answer: "B: To succeed, you must first realize the dream.",
+    image:"assets/images/tinkerbell.gif"
+  },
+  {
+    question: "When did Disneyland open?",
+    options: ["A: August 1, 1961", "B: May 30, 1959", "C: July 17, 1955", "D: June 14, 1949"],
+    answer: "C: July 17, 1955",
+    image:"assets/images/tinkerbell.gif"
+  },
+  {
+    question: "Which of these celebrities did not host at least part of the opening day Disneyland broadcast?",
+    options: ["A: Art Linkletter", "B: Ronald Reagan", "C: Kirk Douglas", "D: Irene Dunne"],
+    answer: "C: Kirk Douglas",
+    image:"assets/images/kirk_douglas.gif"
+  },
+  {
+    question: "Which was not one of the opening day rides at Disneyland?",
+    options: ["A: Autopia", "B: Mad Tea Party", "C: Dumbo the Flying Elephant", "D: The Jungle Cruise"],
+    answer: "C: Dumbo the Flying Elephant",
+    image:"assets/images/dumbo.gif"
+  },
+  {
+    question: "What terminal illness did Walt Disney suffer from",
+    options: ["A: Lung Cancer", "B: Heat Stroke", "C: Pneumonia", "D: Brain Cancer"],
+    answer: "A: Lung Cancer",
+    image:"assets/images/lungs.gif"
+  }];
+
+// ----------------------------------------Functions------------------------------------
+
+var quiz = {
+  questions: questions,
+  currentQuestion: 0,
+  currentImage: 0,
+  counter: 30,
+  correct: 0,
+  incorrect: 0,
+  unanswered: 0,
+
+
+  countdown: function () {
+    quiz.counter--;
+    $("#counter").html(quiz.counter);
+    if (quiz.counter <= 0) {
+      console.log("TIME UP!");
+      quiz.timeUp();
+    }
+  },
+
+  loadQuestion: function () {
+    timer = setInterval(quiz.countdown, 1000);
+    $("#gameDisplay").html("<h2> Time remaining <span id='counter'> 30 </span> seconds</h2>");
+    $("#gameDisplay").append("<h2>" + questions[quiz.currentQuestion].question + "</h2>");
+    //For loop to dynamically create buttons equal to the options.
+    for (var i = 0; i < questions[quiz.currentQuestion].options.length; i++) {
+      $("#gameDisplay").append("<button class='option-button' id='button-" + i + "' data-name='" + questions[quiz.currentQuestion].options[i] + "' >" +
+        questions[quiz.currentQuestion].options[i] + "</button>");
+    }
+  },
+
+  displayImage: function () {
+    $("#imageDisplay").append("<img src=" + questions[quiz.currentQuestion].image + ">");
+  },
+
+  nextQuestion: function () {
+    quiz.counter = 30;
+    $("#counter").html(quiz.counter);
+    quiz.currentQuestion++;
+    quiz.loadQuestion();
+    $("#imageDisplay").empty();
+  },
+
+  timeUp: function () {
+    clearInterval(timer);
+    quiz.unanswered++;
+    $("#gameDisplay").html("<h2>You're out of time!</h2>");
+    $("#gameDisplay").append("<h3>The answer you were looking for is " +
+      questions[quiz.currentQuestion].answer + "</h3>");
+    quiz.displayImage();
+
+    if (quiz.currentQuestion == questions.length - 1) {
+      $("#imageDisplay").empty();
+      setTimeout(quiz.results, 3 * 1000);
+      
+    }
+    else {
+      setTimeout(quiz.nextQuestion, 3 * 1000);
+    }
+  },
+  results: function () {
+    clearInterval(timer);
+    $("#gameDisplay").html("<h2>All done!</h2>");
+    $("#gameDisplay").append("<h3>Correct answers: " + quiz.correct + "</h3>");
+    $("#gameDisplay").append("<h3>Incorrect answers: " + quiz.incorrect + "</h3>");
+    $("#gameDisplay").append("<h3>Unaswered: " + quiz.unanswered + "</h3>");
+    $("#gameDisplay").append("<button id='reset'> RESET</button>");
+    $("#imageDisplay").empty();
+  },
+
+  clicked: function (e) {
+    clearInterval(timer);
+    if ($(e.target).data("name") == questions[quiz.currentQuestion].answer) {
+      quiz.answeredCorrectly();
+    }
+    else {
+      quiz.answeredIncorrectly();
+    }
+  },
+
+  answeredCorrectly: function () {
+    clearInterval(timer);
+    quiz.correct++;
+    $("#gameDisplay").html("<h2>You got it right!</h2>");
+    //append image at current index
+    quiz.displayImage();
+
+    if (quiz.currentQuestion == questions.length - 1) {
+      setTimeout(quiz.results, 3 * 1000);
+    }
+    else {
+      setTimeout(quiz.nextQuestion, 3 * 1000);
+    }
+  },
+
+  answeredIncorrectly: function () {
+    clearInterval(timer);
+    quiz.incorrect++;
+    $("#gameDisplay").html("<h2>Wrong answer!</h2>");
+    $("#gameDisplay").append("<h3>The answer you were looking for is " +
+      questions[quiz.currentQuestion].answer + "</h3>");
+    quiz.displayImage();
+
+    if (quiz.currentQuestion == questions.length - 1) {
+      setTimeout(quiz.results, 3 * 1000);
+    }
+    else {
+      setTimeout(quiz.nextQuestion, 3 * 1000);
+    }
+  },
+
+
+  reset: function () {
+    $("#imageDisplay").empty();
+    quiz.currentQuestion = 0;
+    quiz.counter = 15;
+    quiz.correct = 0;
+    quiz.incorrect = 0;
+    quiz.unanswered = 0;
+    quiz.loadQuestion();
+  }
+
+}
+
 
